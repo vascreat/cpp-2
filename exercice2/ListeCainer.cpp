2e092bcf4f30d6ee4a52c1d73afe197347c23a84
nazimMint <nazim071305@gmail.com>
2025-09-23 15:35:37 +0000

    sauvegarde

--- exercice2/ListeCainer.cpp
+++ exercice2/ListeCainer.cpp
@@ -16,7 +16,9 @@ class Noeud {
 void afficherListe(Noeud* tete);
 void ajouterDebut(Noeud*& tete, int valeur);
 void ajouterFIn(Noeud*& tete, int valeur);
-
+bool rechercher(Noeud* tete, int valeur);
+bool supprimerElement(Noeud*& tete, int valeur);
+void compterElements(Noeud* tete);
 
 int main() {
     // TODO: Créer 3 nœuds et les relier
@@ -35,9 +37,71 @@ int main() {
     afficherListe(n1);
     ajouterFIn(n1, 40);
     afficherListe(n1);
+    cout << rechercher(n1, 20) << endl;
+    cout << rechercher(n1, 100) << endl;
+    compterElements(n1);
+    cout << supprimerElement(n1, 20) << endl;
+    cout << supprimerElement(n1, 40) << endl;
+    cout << supprimerElement(n1, 5) << endl;
+    afficherListe(n1);
+    compterElements(n1);
+
 
     return 0;
 }
+
+void compterElements(Noeud* tete){
+    Noeud* courant = tete;
+    int compteur = 0;
+    while(courant != nullptr){
+        compteur++;
+        courant = courant->suivant;
+    }
+    cout << "Nombre d'elements dans la liste : " << compteur << endl;
+}
+
+bool supprimerElement(Noeud*& tete, int valeur){
+    Noeud* courant = tete;
+    Noeud* precedant = nullptr;
+
+    while(courant != nullptr){
+
+        if(courant->valeur == valeur){
+            cout << "trouver a tete :" << courant->valeur << endl;
+            tete = courant->suivant;
+            courant->suivant = nullptr;
+            return true;
+        }
+
+        precedant = courant;
+        courant = courant->suivant;
+        cout << "avant :" << precedant->valeur << endl;
+        cout << "currant :" << courant->valeur << endl;
+
+        if(courant->valeur == valeur){
+            cout << "found :" << courant->valeur << endl;
+            precedant->suivant = courant->suivant;
+            courant = courant->suivant = nullptr;
+            return true;
+        }
+
+    }
+
+    return false;
+
+}
+
+bool rechercher(Noeud* tete, int valeur) {
+    Noeud* courant = tete;
+    while (courant != nullptr) {
+        if (courant->valeur == valeur) {
+            return true; // Valeur trouvée
+        }
+        courant = courant->suivant;
+    }
+    return false; // Valeur non trouvée
+}
+
 
 void ajouterFIn(Noeud*& tete, int valeur){
     Noeud* nouveauNoeud = new Noeud(valeur);
